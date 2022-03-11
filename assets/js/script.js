8b957bf765ccfc4b683a4cc292c60152ee3af069
Achu Ajesh <achuv37@gmail.com>
2022-03-11 08:18:17 -0500

    Add pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-alert("hello");
+// Pseudo code
+// 1. Display the Current day at the top of the calender.
+// 2. Display the Day planner presented with the time blocks for standard business hours.
+// 3. Create Time, Textarea, Save button for the time block section.
+// 4. Allow us to enter a event in the text area section, when it clicked.
+// 5. Display color-coded time blocks to indicate whether it is in past,present or future.
+// 6. Set the local storage.
+// 7. Saved events persist, when the page reloaded again.
