0c9c99fe9cbec4a7e6052a533f37b3a6155089e2
Achu Ajesh <achuv37@gmail.com>
2022-03-15 11:03:22 -0400

    Add comments to the script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,44 +21,46 @@ function displayTime() {
 }
 // Display the current date and time every seconds.
   setInterval(displayTime, 1000);
-    
-  
-
-// We need to create a Time block: for loop to iterate through the showTime and timeId arrays.
-  for (var i = 0; i <showTime.length; i++) {
+// Function to create Planner
+  function createPlanner () {
+  // We need to create a Time block: for loop to iterate through the showTime and timeId arrays.
+    for (var i = 0; i <showTime.length; i++) {
 // Creating row for time block. 
-    var rowEl = $("<div class='row time-block'>");
-    rowEl.attr("id", timeId[i]);
+      var rowEl = $("<div class='row time-block'>");
+      rowEl.attr("id", timeId[i]);
 // Creating a div for showing time.
-    var timeEl = $("<div class='hour col-1'>");
+      var timeEl = $("<div class='hour col-1'>");
 // Time div shows the time from 9 am to 5pm.
-    timeEl.text(showTime[i]);
+      timeEl.text(showTime[i]);
 // Append the time division to the row
-    rowEl.append(timeEl);
+      rowEl.append(timeEl);
 // Creating a textarea for user to enter events.
-    var textareaEl = $("<textarea class='col-10'>");
-    textareaEl.attr("id", showTime[i]);
-    textareaEl.text();
+      var textareaEl = $("<textarea class='col-10'>");
+      textareaEl.attr("id", showTime[i]);
+      textareaEl.text();
 // Append textarea to the row.
-    rowEl.append(textareaEl);
+      rowEl.append(textareaEl);
 // Creating the save button for the events to save.
-    var buttonEl = $("<button type='button' class ='saveBtn col-1 fas fa-save'>");
-    buttonEl.text();
+      var buttonEl = $("<button type='button' class ='saveBtn col-1 fas fa-save'>");
+      buttonEl.text();
 // Appends the button element to the row
-    rowEl.append(buttonEl);
+      rowEl.append(buttonEl);
 // Appends the row to the container.
-    containerEl.append(rowEl);
-}
+      containerEl.append(rowEl);
+    }
+  }
+  createPlanner(); 
+// Calling localStorageEvent function.
   localStorageEvents();
 // Creating local storage function: for the events to persist.
   function localStorageEvents () {
   // For loop to iterate through the length of showTime.
       for (var index=0; index<showTime.length; index++) {
   // Getting data from local storage.
-      $("textarea")[index].value = localStorage.getItem("localStorageEventList" + String(index +1));
+        $("textarea")[index].value = localStorage.getItem("localStorageEventList" + String(index +1));
       
     }
-    }
+  }
   
 // Adding eventListener to All the button elements.
   $("button").on("click", function (event) {
